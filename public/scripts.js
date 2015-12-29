f476d22ba2b5b7d23e79566ca581523d2b3af9a7
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-12-29 18:22:37 +0100

    Adds some configuration

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   var quoteLabel = document.getElementById("quote-label");
+  var authorLabel = document.getElementById("author-label");
 
   for (var i = 0; i < window.quotes.length; i++) {
     quoteLabel.textContent = window.quotes[i].quote;
   }
+
+  var quoteWidth = parseFloat(style.getPropertyValue('margin-left').replace('px', ''))
+  var quoteLeftMargin = parseFloat(style.getPropertyValue('width').replace('px', ''))
+  var leftMargin = quoteWidth + quoteLeftMargin
+
+  console.log(leftMargin)
+
+  authorLabel.style.marginLeft = leftMargin + "px"
 })
