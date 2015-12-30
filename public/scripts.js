a9cd25bfadcec9f3b44bb7acaf385d33ace0ba6a
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-12-30 11:31:31 +0100

    Adds some more information for the JS code

--- public/scripts.js
+++ public/scripts.js
@@ -4,23 +4,31 @@ document.addEventListener("DOMContentLoaded", function() {
   var authorLabel = document.getElementById("author-label");
   var body = document.body;
   var quotes = window.quotes;
+  var colors = ["#BCF378", "#FFABD0", "#4FBDF2", "#4AF2A1", "#E3C18A"];
 
   function presentQuote() {
     var quoteNumber = Math.floor(Math.random() * quotes.length);
+    var colorNumber = Math.floor(Math.random() * colors.length);
+    var color = colors[colorNumber];
 
     quoteLabel.textContent = quotes[quoteNumber].quote;
     authorLabel.textContent = quotes[quoteNumber].author;
+    body.style.backgroundColor = color;
 
     authorLabel.style.width = quoteLabel.offsetWidth + "px";
   }
 
+  var quoteInterval = null;
+
   function buildQuoteInterval() {
-    return window.setInterval(presentQuote, 2500);
+    window.clearInterval(quoteInterval)
+    presentQuote();
+    quoteInterval = window.setInterval(presentQuote, 3500);
   }
 
   window.setTimeout(function() {
-    body.addEventListener("click", function() { presentQuote(); });
+    body.addEventListener("click", buildQuoteInterval);
   }, 1500);
 
-  presentQuote();
+  buildQuoteInterval();
 })
