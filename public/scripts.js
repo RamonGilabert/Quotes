b4e7fc9f7e8d1d7e8c0d99392b100a265f08a0ff
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-12-30 11:18:49 +0100

    Adds some code for the click listening

--- public/scripts.js
+++ public/scripts.js
@@ -2,11 +2,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var quoteLabel = document.getElementById("quote-label");
   var authorLabel = document.getElementById("author-label");
+  var body = document.body;
+  var quotes = window.quotes;
 
-  for (var i = 0; i < window.quotes.length; i++) {
-    quoteLabel.textContent = window.quotes[i].quote;
-    authorLabel.textContent = window.quotes[i].author;
+  function presentQuote() {
+    var quoteNumber = Math.floor(Math.random() * quotes.length);
+
+    quoteLabel.textContent = quotes[quoteNumber].quote;
+    authorLabel.textContent = quotes[quoteNumber].author;
+
+    authorLabel.style.width = quoteLabel.offsetWidth + "px";
   }
 
-  authorLabel.style.width = quoteLabel.offsetWidth + "px";
+  function buildQuoteInterval() {
+    return window.setInterval(presentQuote, 2500);
+  }
+
+  window.setTimeout(function() {
+    body.addEventListener("click", function() { presentQuote(); });
+  }, 1500);
+
+  presentQuote();
 })
